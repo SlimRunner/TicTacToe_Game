4721d1dd4118b60f537fe28086e471e515c4f52a
Mario D Flores <43660822+SlimRunner@users.noreply.github.com>
2019-01-15 16:32:03 -0800

    minor additions
    Added a couple of functions to randomly pick values as well as a hard coded first move for the AI when it picks first.

--- TicTacToe/TicTacToe/TicTacToe_AI.cpp
+++ TicTacToe/TicTacToe/TicTacToe_AI.cpp
@@ -4,6 +4,14 @@
 #include <cstdlib>
 #include <ctime>
 
+template <class T, class ... Args> T rand_select(T, Args ...) noexcept;
+template <class T> T rand_ratio_pick(T, T, float);
+
+constexpr float operator "" _per(long double percent)
+{
+	return static_cast<float>(percent / 100);
+}
+
 enum class tic::AI_Level
 {
 	AI_EASY = 0,
@@ -15,14 +23,14 @@ tic::TicTacToe_AI::TicTacToe_AI() :
 	m_myDiff(AI_Level::AI_MEDIUM)
 {
 	//nothing else to do
-	/*TEMP*/srand(time(NULL));
+	/*TEMP*/srand(static_cast<unsigned int>(time(NULL)));
 }
 
 tic::TicTacToe_AI::TicTacToe_AI(AI_Level difficulty) :
 	m_myDiff(difficulty)
 {
 	//nothing else to do
-	/*TEMP*/srand(time(NULL));
+	/*TEMP*/srand(static_cast<unsigned int>(time(NULL)));
 }
 
 tic::TicTacToe_AI::~TicTacToe_AI()
@@ -30,26 +38,54 @@ tic::TicTacToe_AI::~TicTacToe_AI()
 	//nothing to do
 }
 
-t3g::cell_loc tic::TicTacToe_AI::get_move(t3g::T3_Match & currMatch)
+t3g::cell_loc tic::TicTacToe_AI::get_move(const t3g::T3_Match & currMatch) const
 {
 	/*  ALL THE CODE INSIDE THIS DEFINITION IS TEMPORARY  */
-
-	//bool found = false;
-	t3g::cell_loc randLoc;
-
-	/*do
+	t3g::cell_loc aiSel;
+	
+	if (currMatch.get_board_status() == t3g::T3_board_state::EMPTY_BOARD)
 	{
-		randLoc = rand() % t3g::BOARD_SIZE;
-		if (currMatch.get_cell_state(randLoc) == t3g::T3_cell_state::NULL_STATE)
+		aiSel = rand_select(0, 2, 6, 8); //selects any of the corners
+		aiSel = rand_ratio_pick(static_cast<int>(aiSel), 4, 80.0_per); //80% of the time stays with corners, otherwise it will select the center
+	}
+	else
+	{
+		//do recursive function here
+		do
 		{
-			found = true;
-		}
-	} while (!found);*/
+			aiSel = rand() % t3g::BOARD_SIZE;
+			//get offense map
+			//get defense map
+			//randLoc = 0;
+		} while (currMatch.get_cell_state(aiSel) != t3g::T3_cell_state::NULL_STATE);
+	}
 
-	do
+	return aiSel;
+}
+
+template<class T, class ... Args>
+T rand_select(T first, Args ... args) noexcept
+{
+	T arr[sizeof...(args) + 1] = { first, args... };
+
+	return arr[rand() % (sizeof...(args) + 1)];
+}
+
+template <class T>
+T rand_ratio_pick(T in1, T in2, float ratio)
+{
+	if (ratio >= 1) return in1; //one is always true
+	if (ratio <= 0) return in2; //zero is always false
+
+	int threshold = static_cast<int>((RAND_MAX + 1) * ratio);
+	int output = rand();
+
+	if (output < threshold)
 	{
-		randLoc = rand() % t3g::BOARD_SIZE;
-	} while (currMatch.get_cell_state(randLoc) != t3g::T3_cell_state::NULL_STATE);
-
-	return randLoc;
+		return in1;
+	}
+	else
+	{
+		return in2;
+	}
 }
--- TicTacToe/TicTacToe/TicTacToe_AI.h
+++ TicTacToe/TicTacToe/TicTacToe_AI.h
@@ -1,10 +1,11 @@
 #ifndef TICTACTOE_AI_H
 #define TICTACTOE_AI_H
 
+//Forward declarations in the t3g namespace
 namespace t3g
 {
-	class T3_Match;
-	typedef unsigned int cell_loc;
+	class T3_Match; //forward declaration
+	typedef unsigned int cell_loc; //re-declaration
 }
 
 namespace tic
@@ -18,8 +19,7 @@ namespace tic
 		TicTacToe_AI(AI_Level difficulty);
 		~TicTacToe_AI();
 
-		/*Does this work?*/
-		t3g::cell_loc get_move(t3g::T3_Match & currMatch);
+		t3g::cell_loc get_move(const t3g::T3_Match & currMatch) const;
 
 	private:
 		AI_Level m_myDiff;
