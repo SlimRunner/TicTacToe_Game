b5cceeeed38936cc8d4ac51dcc393d83a4749759
Mario D Flores <43660822+SlimRunner@users.noreply.github.com>
2019-01-15 17:14:44 -0800

    Added TODOs and Structure
    Descriptive comments and switches to handle difficulty were added. AI logic is still missing.

--- TicTacToe/TicTacToe/TicTacToe_AI.cpp
+++ TicTacToe/TicTacToe/TicTacToe_AI.cpp
@@ -41,12 +41,35 @@ tic::TicTacToe_AI::~TicTacToe_AI()
 t3g::cell_loc tic::TicTacToe_AI::get_move(const t3g::T3_Match & currMatch) const
 {
 	/*  ALL THE CODE INSIDE THIS DEFINITION IS TEMPORARY  */
+	t3g::cell_loc badStart = rand_select(1, 3, 5, 7);
 	t3g::cell_loc aiSel;
 	
 	if (currMatch.get_board_status() == t3g::T3_board_state::EMPTY_BOARD)
 	{
-		aiSel = rand_select(0, 2, 6, 8); //selects any of the corners
-		aiSel = rand_ratio_pick(static_cast<int>(aiSel), 4, 80.0_per); //80% of the time stays with corners, otherwise it will select the center
+		aiSel = rand_select(0, 2, 6, 8); //pre-selects any of the corners
+
+		switch (m_myDiff)
+		{
+		case tic::AI_Level::AI_EASY:
+			//20% of the time stays with corners, otherwise it will select the center
+			aiSel = rand_ratio_pick(static_cast<int>(aiSel), 4, 20.0_per);
+			//30% of the time stays with good start, otherwise it will select at random among bad choices
+			aiSel = rand_ratio_pick(static_cast<int>(aiSel), static_cast<int>(badStart), 30.0_per);
+
+			break;
+		case tic::AI_Level::AI_MEDIUM:
+			//20% of the time stays with corners, otherwise it will select the center
+			aiSel = rand_ratio_pick(static_cast<int>(aiSel), 4, 20.0_per);
+			//95% of the time stays with good start, otherwise it will select at random among bad choices
+			aiSel = rand_ratio_pick(static_cast<int>(aiSel), static_cast<int>(badStart), 95.0_per);
+			break;
+		case tic::AI_Level::AI_HARD:
+			//90% of the time stays with corners, otherwise it will select the center
+			aiSel = rand_ratio_pick(static_cast<int>(aiSel), 4, 90.0_per);
+			break;
+		default:
+			break;
+		}
 	}
 	else
 	{
@@ -54,9 +77,23 @@ t3g::cell_loc tic::TicTacToe_AI::get_move(const t3g::T3_Match & currMatch) const
 		do
 		{
 			aiSel = rand() % t3g::BOARD_SIZE;
-			//get offense map
-			//get defense map
-			//randLoc = 0;
+			/*selMap = get_minimax_map(currMatch)*/
+			switch (m_myDiff)
+			{
+			case tic::AI_Level::AI_EASY:
+				/*Select index which contains 1 rarely unless there is none, in that case select index with 0*/
+				/*often select index with -1*/
+				break;
+			case tic::AI_Level::AI_MEDIUM:
+				/*Select index which contains 1 sometimes unless there is none, in that case select index with 0*/
+				/*sometimes select index with -1*/
+				break;
+			case tic::AI_Level::AI_HARD:
+				/*Select index which contains 1 always unless there is none, in that case select index with 0*/
+				break;
+			default:
+				break;
+			}
 		} while (currMatch.get_cell_state(aiSel) != t3g::T3_cell_state::NULL_STATE);
 	}
 
